b18451a2aca6fd0c8130d204e63db014e4ab6887
hghg01 <102838081+hghg01@users.noreply.github.com>
2024-08-13 08:38:32 -0300

    ,

--- script.js
+++ script.js
@@ -58,10 +58,10 @@ function mostraAlternativas() {
     for (const alternativa of perguntaAtual.alternativas) {
         const botaoAlternativa = document.createElement("button");
         botaoAlternativa.textContent = alternativa.texto;
-        botaoAlternativa.addEventListener("click", function{}
+        botaoAlternativa.addEventListener("click", function(){
         atual ++;
-        mostraPerguntas():
-    )
+        mostraPerguntas();
+    })
         caixaAlternativas.appendChild(botaoAlternativa);
 
     
