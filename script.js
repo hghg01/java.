e76b5ae426ca2061c6da8b21371c40aa93b7c195
hghg01 <102838081+hghg01@users.noreply.github.com>
2024-08-20 08:43:33 -0300

    .

--- script.js
+++ script.js
@@ -10,11 +10,11 @@ const perguntas = [
         enunciado: "No âmbito social, sua vó faleceu e não deixou testamento pronto. O que você faz? ",
         alternativas: [{
             texto: "Furta tudo que há de valor e foge com toda a herança para si mesmo. Sua família toda morre de miséria, mas você se casa e cria uma nova família.",
-            afirmacao: "é "
+            afirmacao: "Você é um pilantra."
         },
         {
             texto: "Propõe uma divisão igualitária de todos os bens entre sua família. Mas fica com quase nada da herança e sua família te odeia.",
-            afirmacao: "Afirmação 2"
+            afirmacao: "Você é trouxa."
         },]
     },
 
@@ -22,11 +22,11 @@ const perguntas = [
         enunciado: "No âmbito ambiental, você está em Colombo e vê uma queimada. O que você faz? ",
         alternativas: [{
             texto: "Descarta a bituca do seu cigarro naqueimada, aumentando-a. Mas o cheiro dafumaca te causa ânsia e você nunca maisconsegue fumar, o que evita um câncer depulmão.",
-            afirmacao: "Afirmação 3"
+            afirmacao: "Você é porco."
         },
         {
             texto: "Liga pra instituição de proteção ao meioambiente do município. Mas eles acham que foi você quem aumentou a queimada porque você estava com um cigarro namão.",
-            afirmacao: "Afirmação 4"
+            afirmacao: "Bem feito."
         },]
     },
 
@@ -34,11 +34,11 @@ const perguntas = [
         enunciado: "No âmbito tecnólogico, você possui controle da maior empresa de IA do mundo. O que você escolhe? ",
         alternativas: [{
             texto: "Fazer a tecnologia ser uma mão amiga na realização de trabalhos mais pesados e fornecer assistência para diminuir o peso no trabalho humano. Perde o emprego e vira mendigo.",
-            afirmacao: "Afirmação 5"
+            afirmacao: "Desempregado."
         },
         {
             texto: " Focar na replica e produção de arte, dando liberdade para explorar os limites da criatividade artificial, assim aumentando a carga horaria trabalhada dos humanos. Se torna a pessoa mais rica do mundo.",
-            afirmacao: "Afirmação 6"
+            afirmacao: "Você é uma pessoa HORRÍVEL."
         },
         ]
     }
